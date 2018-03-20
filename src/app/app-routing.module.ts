1dd596a7eb0a66ac3d4e7a46b88f9fb87953eafa
Helder <helder@yarilabs.com>
2018-03-20 11:40:58 +0000

    add lazy loading angular 5

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FirstComponent } from './first/first.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: FirstComponent,
+  pathMatch: 'full'
+}, {
+  path: 'lazy',
+  loadChildren: 'app/lazy/lazy.module#LazyModule'
+}];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, {
+    useHash: Boolean(history.pushState) === true,
+    }),
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
